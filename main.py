762cafca673162bd21cbe968eead4ac8ec321485
Ubuntu <ubuntu@ip-172-31-42-183.ec2.internal>
2020-09-30 03:05:51 +0000

    broken code

--- main.py
+++ main.py
@@ -11,9 +11,27 @@ import pandas
 import requests
 import requests_html
 import yahoo_fin.stock_info as yahoo
+from yahoo_finance import Share
+import yfinance as yf
+
 import pdb
+import colorama
+import datetime
+from colorama import Fore, Back
 
-
+colorama.init(autoreset=True)
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+   
 
 # To skip welcome message and go to instructions and entering stock ticker
 if "--help" in sys.argv:
@@ -32,31 +50,36 @@ else:
 Please follow the instructions at each input step and at any of the inputs if you would like to exit the program, just enter 'exit' as the input.
 """)
 
-
 # main code
-
-response = yahoo.get_live_price("aapl")
-
-print(response)
-
-
 ## search testing
-
-stock1 = input("please enter a stock ticker")
-print(f""
-"")
-print(yahoo.get_live_price(stock1))
-print(f""
-"")
+class Yahoo():
+    def Prices(self, symbol):
+        return yahoo.get_live_price(symbol)
+        
 
 
-###Classes 
+symbol =  input(f"""please enter a stock ticker 
+""")
+print(f"""{yahoo.get_live_price(symbol)}
+""")
+print(Yahoo().Prices(symbol))
+
+###Classes GENERAL
 class Person:
   def __init__(self, name, age):
     self.name = name
     self.age = age
-
 p1 = Person("John", 36)
-
 print(p1.name)
 print(p1.age)
+
+
+### Yahoo Classes
+
+
+msft = yf.Ticker(symbol)
+company_name = msft.info['longName']
+
+print(company_name)
+
+#Output = 'Microsoft Corporation'
