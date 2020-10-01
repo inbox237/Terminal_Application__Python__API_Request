41f3917000e632ef0a360ad5470c7bc9e583ff35
Ubuntu <ubuntu@ip-172-31-42-183.ec2.internal>
2020-10-01 06:39:08 +0000

    fixed some pep8 styling

--- main.py
+++ main.py
@@ -2,21 +2,9 @@
 
 import sys
 from prettytable import PrettyTable
-import requests
-import json
-import ftplib
-import io
-import pandas
-import requests
-import requests_html
-import yahoo_fin.stock_info as yahoo
-from yahoo_finance import Share
-import yfinance as yf
-import argparse
-import pdb
 import colorama
 import datetime
-from colorama import Fore, Back
+from colorama import Fore
 from yahoo_fin.stock_info import get_data
 from Average import Average
 from Yahoo import Yahoo
@@ -27,21 +15,24 @@ colorama.init(autoreset=True)
 
 # To skip welcome message and go to instructions and entering stock ticker
 if "--help" in sys.argv:
-   print("""
-   Here are the instructions for running the app:
-   Please follow the instructions at each input step and at any of the inputs if you would like to exit the program, just enter 'exit' as the input.
-   """)
+    print("Here are the instructions for running the app:")
+    print("Please follow the instructions at each input step")
+    print("and at any of the inputs if you would like to exit")
+    print("the program, just enter 'exit' as the input.")
 
 elif "--ticker" in sys.argv:
-   option_index = sys.argv.index("--ticker")
-   ticker = sys.argv[option_index + 1]
+    option_index = sys.argv.index("--ticker")
+    ticker = sys.argv[option_index + 1]
 
-#Welcome message
+# Welcome message
 else:
-   print("""Welcome, this is Joel's Yahoo finance stock price checker.
-   
-Please follow the instructions at each input step and at any of the inputs if you would like to exit the program, just enter 'exit' as the input.
-""")
+    print("Welcome, this is Joel's Yahoo finance stock price checker.")
+    print("Please follow the instructions at each step.")
+    print("If you would like to exit the program, type 'exit'.\n")
+    print("To get the latest LIVE price and an average of the last 30 days of")
+    print("any Stock please enter a stock ticker.")
+    print("For example, Apple Incorporated would be entered as 'AAPL'")
+
 
 # MAIN CODE
 def main():
@@ -49,41 +40,32 @@ def main():
         ticker = None
         while ticker is None:
             try:
-                ticker = input ("""To get the latest LIVE price and an average of the last 30 days of any Stock please enter a stock ticker, for example, Apple Incorporated would be entered as 'AAPL'. Or to exit the app at any time, simply type 'exit'.
-
-""")
-
+                ticker = input("\nPlease enter a valid Stock Ticker:\n")
                 if ticker == "exit":
-                   print("Program is now exiting, thank you...")
-                   return
-                check_ticker = yahoo.get_live_price(ticker)
-                
-                
+                    print("Program is now exiting, thank you...")
+                    return
+
                 yahoo_master = Yahoo()
-                
-                ##Dates Defined
+                # Dates Defined
                 start_time = datetime.datetime.now() - datetime.timedelta(30)
-                start_date = start_time.strftime("%m/%d/%Y")
-                end_time = datetime.datetime.today().strftime("%m/%d/%Y")
                 end_date_notime = datetime.datetime.now().strftime("%m/%d/%Y")
                 start_date_notime = start_time.strftime("%m/%d/%Y")
-                end_pd = datetime.datetime.now().strftime("%Y-%m-%d")
-                start_pd = start_time.strftime("%Y-%m-%d")
-                ticker_days= get_data(ticker, start_date=start_date_notime, end_date=end_date_notime, index_as_date = True, interval="1d")
+                
+                
+                ticker_days = get_data(ticker, start_date=start_date_notime, end_date=end_date_notime, index_as_date = True, interval="1d")
                     
-                    
-
-                        
+                data_master = Get_data()    
                 average_master = Average()
                 
-                #OUTPUT TABLE
+                # OUTPUT TABLE
                 d = PrettyTable()
-                d.field_names = ([Color.BOLD +Color.UNDERLINE + "Company" + Color.END, Color.BOLD +Color.UNDERLINE + "Current LIVE Price" +Color.END, Color.BOLD +Color.UNDERLINE + "Average Price 30 Days" +Color.END])
+                d.field_names = ([Color.BOLD +Color.UNDERLINE + "Company" + Color.END , Color.BOLD + Color.UNDERLINE + "Current LIVE Price" +Color.END, Color.BOLD +Color.UNDERLINE + "Average Price 30 Days" +Color.END])
                 d.add_row([Color.BOLD +Color.BLUE +  f"{ticker.upper()} - {yahoo_master.company_name(ticker)}"+ Color.END,Color.BOLD +Color.GREEN + "$""{:.2f}".format(yahoo_master.price(ticker))+Color.END,"$""{:.2f}".format(average_master.ticker_average(ticker_days))+Color.END])
                 print(d)
                 
             except:
                 print(Fore.RED + Color.BOLD +"Ticker must be a valid Stock Ticker, please try again.")
+                
                 
 main()
 
--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-from main import yahoo_master.company_name as company_name
-from main import yahoo_master.price as price
-from main import average_master.ticker_average as ticker_average
+from Average import Average
+from Yahoo import Yahoo
+from Color import Color
+import unittest
 
-
-company_name_result = company_name("AAPL")
-if company_name_result != "Apple inc.":
-    raise Exception("Company Name did not bring back Apple Inc. when given 'AAPL' as input!")
-print("did i run")
+class Testing(unittest.TestCase):
+    def test_company_name(self):
+        yahoo_master = Yahoo()
+        result = yahoo_master.company_name("AAPL")
+        expectation = "Apple Inc."
+        
+        self.assertEqual(result, expectation)
+        
+        
+        
